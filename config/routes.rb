b49527cc5a3fbc95f9ce0a76be399060e6e99e9e
Joe Lee <zvmboni@gmail.com>
2022-06-03 03:45:02 -0700

    test

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,13 @@
 Rails.application.routes.draw do
   
-  # Routing logic: fallback requests for React Router.
-  # Leave this here to help deploy your app later!
-  get "*path", to: "fallback#index", constraints: ->(req) { !req.xhr? && req.format.html? }
-
   resources :listing, :review, :user
   post "/login", to: "session#create"
   post "/signup", to: "user#create"
   delete "/logout", to: "session#destroy"
+
+  # Routing logic: fallback requests for React Router.
+  # Leave this here to help deploy your app later!
+  get "*path", to: "fallback#index", constraints: ->(req) { !req.xhr? && req.format.html? }
+
 end
+
